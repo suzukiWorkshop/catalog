9670696e4edf8dc6303750023f22568f6e82025e
Yuki SUZUKI <suzuki@hoccs.co.jp>
2012-10-23 17:26:07 +0900

    [#38240963] 画像を表示できるように修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :price, :image, :image_cache
+  mount_uploader :image, ProductImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023071559) do
+ActiveRecord::Schema.define(:version => 20121023080518) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "price",       :default => 0
+    t.string   "image"
   end
 
 end
