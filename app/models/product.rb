7c49555fe8c7cdc470418439e4d1b5136129eaf6
Yuki SUZUKI <suzuki@hoccs.co.jp>
2012-10-24 11:17:27 +0900

    [#38240963] 検証ロジックの追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :image, :image_cache
   mount_uploader :image, ProductImageUploader
+
+  validates_presence_of :name, :description
+  validates_numericality_of :price, :only_integer => true, :greater_than => 0
+  validates_uniqueness_of :name
+  validate :price_must_be_a_multiple_of_hundreds
+
+  private
+  def price_must_be_a_multiple_of_hundreds
+    unless price % 100 == 0
+      errors.add :price, "must be a multiple of hundreds"
+    end
+  end
+
 end
